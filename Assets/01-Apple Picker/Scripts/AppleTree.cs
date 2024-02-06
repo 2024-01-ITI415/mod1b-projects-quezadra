4ab9f2ef5d40e2e2a708f0aa71ae4bb7c90eca14
quezadra <rmq13@scarletmail.rutgers.edu>
2024-02-05 21:32:46 -0500

    9:32
    code for appletree

---
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+
+    //prefab for instantiating apples
+    public GameObject applePrefab;
+
+    //speed at which the apple tree moves
+    public float speed = 1f;
+
+    // distnace where AppleTree turns around
+    public float leftandRightEdge = 10f;
+
+    //chance the AppleTree will change direction 
+
+    public float changeToChangeDirection;
+
+    //rate at which Apples will instantiate 
+    public float SecondsBetweenAppleDrop;
+
+
+  
+    void Start()
+    {
+        // dropping every second 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // basic movement and changing direction
+
+    }
+}
