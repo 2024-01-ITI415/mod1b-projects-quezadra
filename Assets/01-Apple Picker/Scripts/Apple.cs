71df45d82cd5ccb2ef35fb27569321f6904e8af8
quezadra <rmq13@scarletmail.rutgers.edu>
2024-02-06 01:28:08 -0500

    big things happening

--- Assets/01-Apple Picker/Scripts/Apple.cs
+++ Assets/01-Apple Picker/Scripts/Apple.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Apple : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public static float bottomY = -20f; // a
     void Update()
     {
-        
+        if (transform.position.y < bottomY)
+        {
+            Destroy(this.gameObject); // b
+        }
     }
 }
