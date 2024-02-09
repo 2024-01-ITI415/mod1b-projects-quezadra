1df85aa2ccab32a92cd71d9ec8d559f942357deb
quezadra <156867092+quezadra@users.noreply.github.com>
2024-02-08 20:26:47 -0500

    more class

--- Assets/Slingshot.cs
+++ Assets/Slingshot.cs
@@ -4,20 +4,38 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
+    [Header("Set in Inspector")]
+    public GameObject prefabProjectile;
     public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
     private void Awake()
     {
+
         Transform launchPointTrans = transform.Find("LaunchPoint"); //a
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false); //b
+        launchPos = launchPointTrans.position;
     }
     void OnMouseEnter()
     {
         print("Slingshot:OnMouseEnter()");
+        launchPoint.SetActive(true);
 
     }
     void OnMouseExit()
     {
         print("Slingshot: OnMouseExit()");
+        launchPoint.SetActive(false);
+    }
+
+    void OnMouseDown()
+    {
+        aimingMode = true;
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        projectile.transform.position = launchPos;
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
+
     }
 }
