c0959af74785cc54f9b10e0c04c7298852d7dc89
quezadra <rmq13@scarletmail.rutgers.edu>
2024-02-12 22:18:56 -0500

    ball moving

--- Assets/Slingshot.cs
+++ Assets/Slingshot.cs
@@ -6,10 +6,14 @@ public class Slingshot : MonoBehaviour
 {
     [Header("Set in Inspector")]
     public GameObject prefabProjectile;
+    public float velocityMult = 8f;
+    [Header("Set Dynamically")]
     public GameObject launchPoint;
     public Vector3 launchPos;
     public GameObject projectile;
     public bool aimingMode;
+    private Rigidbody projectileRigidbody; 
+
     private void Awake()
     {
 
@@ -36,5 +40,45 @@ public class Slingshot : MonoBehaviour
         projectile = Instantiate(prefabProjectile) as GameObject;
         projectile.transform.position = launchPos;
         projectile.GetComponent<Rigidbody>().isKinematic = true;
+
+        projectileRigidbody = projectile.GetComponent<Rigidbody>(); // a
+        projectileRigidbody.isKinematic = true; // a
     }
+
+    void Update()
+    {
+        
+        if (!aimingMode) return; 
+                                
+        Vector3 mousePos2D = Input.mousePosition; 
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        
+        Vector3 mouseDelta = mousePos3D - launchPos;
+        
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+        if (mouseDelta.magnitude > maxMagnitude)
+        {
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }
+       
+        Vector3 projPos = launchPos + mouseDelta;
+        projectile.transform.position = projPos;
+        if (Input.GetMouseButtonUp(0))
+        { 
+            aimingMode = false;
+            projectileRigidbody.isKinematic = false;
+            projectileRigidbody.velocity = -mouseDelta * velocityMult;
+            FollowCam.POI = projectile;
+            projectile = null;
+
+
+
+        }
+    }
+
+
+
+
 }
