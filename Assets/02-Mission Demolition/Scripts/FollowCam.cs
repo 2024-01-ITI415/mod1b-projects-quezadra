33b3e813e8339c39e960acc365ae1314abd490be
quezadra <rmq13@scarletmail.rutgers.edu>
2024-02-12 22:50:05 -0500

    save

--- Assets/02-Mission Demolition/Scripts/FollowCam.cs
+++ Assets/02-Mission Demolition/Scripts/FollowCam.cs
@@ -2,22 +2,27 @@ using UnityEngine;
 using System.Collections;
 public class FollowCam : MonoBehaviour
 {
-    static public GameObject POI; // The static point of interest // a
+    static public GameObject POI; 
+    [Header("Set in Inspector")]
+    public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
+
     [Header("Set Dynamically")]
-    public float camZ; // The desired Z pos of the camera
+    public float camZ; 
     void Awake()
     {
         camZ = this.transform.position.z;
     }
     void FixedUpdate()
     {
-        // if there's only one line following an if, it doesn't need braces
-        if (POI == null) return; // return if there is no poi // b
-                                 // Get the position of the poi
+        
+        if (POI == null) return;                    
         Vector3 destination = POI.transform.position;
-        // Force destination.z to be camZ to keep the camera far enough away
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        destination = Vector3.Lerp(transform.position, destination, easing);
         destination.z = camZ;
-        // Set the camera to the destination
         transform.position = destination;
+        Camera.main.orthographicSize = destination.y + 10;
     }
 }
